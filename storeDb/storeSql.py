fdf80127720ae7c5d9c26aa44821cb4102265b6f
monzoonAsa <asankapinnagoda@gmail.com>
2022-11-25 13:52:53 +0530

    store external csv file in database
    edit storSql.py  to save extenal csv file data in another table on crowd.db

--- storeDb/storeSql.py
+++ storeDb/storeSql.py
@@ -1,100 +1,43 @@
-##mqtt server data log to sqlite DB file by P.G.A.S Pinnagoda 
+##mqtt server data log to sqlite DB file by P.G.A.S Pinnagoda EA3050 Final project
 
-
-
+import csv
+import json
 import sqlite3
 from pathlib import Path
-#recive mqtt data 
-
 import paho.mqtt.client as mqtt
-#from storeData import sensor_Data_Handler
-#from store_Sensor_Data_to_DB import sensor_Data_Handler
 
 # MQTT Settings 
 MQTT_Broker = "192.168.8.100"
 MQTT_Port = 1883
 Keep_Alive_Interval = 45
 MQTT_Topic = "crowdtrace/#"
-
-import json
-import sqlite3
-
-# SQLite DB Name
 DB_Name =  "crowd.db"
+###########################
+###########################
 
 
 
-path_to_file = 'crowd.db' #database file name or path
-path = Path(path_to_file)
+#####################
+#####################
 
-if path.is_file():# to check DB file exist or not
-	print(f"The file {path_to_file} exists")
-else:
-	print(f"The file {path_to_file} does not exist create new DB file..")
-# SQLite DB Name
-	DB_Name =  "crowd.db"
-
-# SQLite DB Table Schema
-	TableSchema="""
-	 drop table if exists crData ;
-	 create table crData(
-  	 id integer primary key autoincrement,
-  	 node text,
-  	 mac text,
-  	 rssi text,
-  	 packets text,
-  	 net_tag text
-  
-	);
-
-	"""	
-
-#Connect or Create DB File
-	conn = sqlite3.connect(DB_Name)
-	curs = conn.cursor()
-
-#Create Tables
-	sqlite3.complete_statement(TableSchema)
-	curs.executescript(TableSchema)
-
-#Close DB
-	curs.close()
-	conn.close()
-
-
-##start listen mqtt data code ------------->>>>>>>
-#=================================================
-#==================================================
-
-mqttc = mqtt.Client()
 
 #Subscribe to all Sensors at Base Topic
 def on_connect(mosq, obj,flags, rc):
-	#print("on connect")
+	print("on connect")
 	mqttc.subscribe(MQTT_Topic)
 #Save Data into DB Table
 def on_message(mosq, obj, msg):
-	#print("mqtt data recived")
+	print("mqtt data recived")
 	data=str(msg.payload.decode("utf-8"))
 	print(data)
 	crowdtraceDataHandler(data)
 
 def on_subscribe(mosq, obj, mid, granted_qos):
- 	#print("on subscribe")
+ 	print("on subscribe")
  	print("Subscription done to MQTT server waiting for data recived....")
  	pass
-
-# Assign event callbacks
-mqttc.on_message = on_message
-mqttc.on_connect = on_connect
-mqttc.on_subscribe = on_subscribe
-
-# Connect
-mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
-
-#=============================================================
-#============================================================
-#=============================================================
+ 	
+#database manage class
 
 class DatabaseManager():
 	def __init__(self):
@@ -112,11 +55,9 @@ class DatabaseManager():
 
 	def __del__(self):
 		self.cur.close()
-		self.conn.close()
-
-
-# Functions to push Sensor Data into Database
-
+		self.conn.close()	
+	
+########################################
 # Function to save data to DB Table
 def crowdtraceDataHandler(jsonData):
 	#Parse Data 
@@ -132,13 +73,103 @@ def crowdtraceDataHandler(jsonData):
 		
 	
 	#Push into DB Table
-	dbObj = DatabaseManager()
-	dbObj.add_del_update_db_record("insert into crData (node,mac, rssi, packets, net_tag) values (?,?,?,?,?)",[node, mac, rssi, packets, net_tag])
-	del dbObj
-	print ("Inserted crowdtrace Data into Database.")
-	print ("")
+	try:
+		dbObj = DatabaseManager()
+		dbObj.add_del_update_db_record("insert into crData (node,mac, rssi, packets, net_tag) values (?,?,?,?,?)",[node, mac, rssi, packets, net_tag])
+		del dbObj
+		print ("Inserted crowdtrace Data into Database.")
+		print ("")
+	except:
+		print("data set alredy in database")
+		pass	
+########################################
+
+# SQLite DB Name
+DB_Name =  "crowd.db"
+# SQLite DB Table Schema
+TableSchema="""
+	 drop table if exists crData ;
+	 create table crData(
+  	 id integer primary key autoincrement,
+  	 node text,
+  	 mac text unique,
+  	 rssi text,
+  	 packets text,
+  	 net_tag text
+  
+	);
+
+	"""	
+#in here unique constant use for not duplicating data on database
+#main program start here------------->>>>>>
 
 
-# Continue the network loop
+path_to_file = 'crowd.db' #database file name or path
+path = Path(path_to_file)
+
+if path.is_file():# to check DB file exist or not
+	print("The databse file exists")
+else:
+	print("The file {path_to_file} does not exist create new DB file..")
+	
+################################################
+
+	
+#create db table
+# SQLite DB Name
+	DB_Name =  "crowd.db"
+	conn = sqlite3.connect(DB_Name)
+	curs = conn.cursor()
+	curs.executescript(TableSchema)
+	
+	
+	curs.close()
+	conn.close()
+	
+###############################################
+#import csv file data to new table
+
+TablePeopleData="""
+	 drop table if exists peopleData ;
+	 create table peopleData(
+  	 name text,
+  	 indexNo text,
+  	 mac text unique
+  	 
+	);
+
+	"""	
+conn = sqlite3.connect(DB_Name)
+curs = conn.cursor()
+curs.executescript(TablePeopleData)	
+print("tabel create done")
+file = open("test.csv")
+print("fle open done")
+content = csv.reader(file)
+insertRecords="insert into peopleData (name,indexNo,mac) values( ?,?,?)"
+curs.executemany(insertRecords, content)
+print("csv data saved")
+conn.commit()
+curs.close()
+conn.close()
+	
+################################################
+
+##start listen mqtt data ------------->
+
+mqttc=mqtt.Client()
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+
+# Connect
+mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
+
+
+########################################
+
+
+
 mqttc.loop_forever()
 
