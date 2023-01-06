a50a2c0a4fd12901f9c946f626e57e20e8b3d3ce
monzoonAsa <asankapinnagoda@gmail.com>
2023-01-06 22:36:42 +0530

    Update reset_conf.lua
    add new function to avoid push button debouncing

--- reset_conf.lua
+++ reset_conf.lua
@@ -21,8 +21,29 @@ function pin_cb()
 end
 
 
+--this new function for avoid pushbutton debounce and when button press once device will restart
+function checkState()
+    print("check state ekata awa")
+    tmr.delay(3000000)
+    btValue=gpio.read(button_pin)
+    print("bt value = ", btValue)
+
+    if btValue==0
+    then
+        print("bt pressed going to pin_cb")
+        pin_cb()
+    else
+        print("button not pressed")
+        node.restart()
+    end
+    
+end
+
 gpio.write(led_pin, value)
 
 -- register a button event
 -- that means, what's registered here is executed upon button event "up"
-gpio.trig(button_pin, "up", pin_cb)
+--gpio.trig(button_pin, "up", pin_cb)
+gpio.trig(button_pin, "down", checkState)
+
+
